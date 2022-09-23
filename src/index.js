0d569c302bebc9f10e98d9dc0b5fad7cb6c42eb8
Luis Tamarez <luistamarez@hotmail.com>
2022-09-22 21:19:04 -0700

    Add logo img

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import "./css/style.css";
 import "./css/normalize.css";
 import "material-icons/iconfont/round.css";
+import logo from "./imgs/drop-down-logo.png";
 import footer from "./footerContent";
 
-// // buildHeaderContent
-// (() => {
-//   const headerDiv = document.createElement("header");
-//   headerDiv.setAttribute("id", "header-content");
-//   const viewListIcon = document.createElement("i");
-//   viewListIcon.classList.add("material-icons-round");
-//   viewListIcon.setAttribute("id", "header-icon");
-//   viewListIcon.textContent = "view_list";
-
-//   const header = document.createElement("h1");
-//   header.innerHTML = "To Do App";
-//   headerDiv.appendChild(viewListIcon);
-//   headerDiv.appendChild(header);
-//   document.body.appendChild(headerDiv);
-// })();
+// buildHeaderContent
+(() => {
+  const logoImg = document.createElement("img");
+  logoImg.src = logo;
+  logoImg.alt = "Logo that reads Phony";
+  const header = document.querySelector(".header");
+  header.insertAdjacentElement("afterbegin", logoImg);
+  // const logoContent = document.query
+  // const headerDiv = document.createElement("header");
+  // headerDiv.setAttribute("id", "header-content");
+  // const viewListIcon = document.createElement("i");
+  // viewListIcon.classList.add("material-icons-round");
+  // viewListIcon.setAttribute("id", "header-icon");
+  // viewListIcon.textContent = "view_list";
+  // const header = document.createElement("h1");
+  // header.innerHTML = "To Do App";
+  // headerDiv.appendChild(viewListIcon);
+  // headerDiv.appendChild(header);
+  // document.body.appendChild(headerDiv);
+})();
 
 // buildPageContent
 (() => {
