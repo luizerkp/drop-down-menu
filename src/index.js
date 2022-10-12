b7d90fd487870252d7f43864aa1fa0d0d06e7215
Luis Tamarez <luistamarez@hotmail.com>
2022-10-11 17:58:11 -0700

    Add drop down animation   * Add complete layout for menu   * Add styling and incons

--- src/index.js
+++ src/index.js
@@ -4,48 +4,51 @@ import "material-icons/iconfont/outlined.css";
 import "./css/style.css";
 import footer from "./footerContent";
 
-// create header content
-const headerContent = (() => {
-  const toggleDropDownDisplay = (id) => {
-    const dropDown = document.querySelector(`[data-id="${id}"]`);
-    // console.log(dropDown);
-    dropDown.classList.toggle("drop-down-links-visible");
-  };
+// // create header content
+// const headerContent = (() => {
+//   const toggleDropDownDisplay = (id) => {
+//     const dropDown = document.querySelector(`[data-id="${id}"]`);
+//     // console.log(dropDown);
+//     dropDown.classList.toggle("drop-down-links-visible");
+//   };
 
-  const toggleDropDownIcon = (icon) => {
-    if (icon.textContent === "arrow_drop_down") {
-      icon.textContent = "arrow_drop_up";
-    } else {
-      icon.textContent = "arrow_drop_down";
-    }
-  };
+//   const toggleDropDownIcon = (icon) => {
+//     if (icon.textContent === "arrow_drop_down") {
+//       icon.textContent = "arrow_drop_up";
+//     } else {
+//       icon.textContent = "arrow_drop_down";
+//     }
+//   };
 
-  return {
-    toggleDropDownDisplay,
-    toggleDropDownIcon,
-  };
-})();
+//   return {
+//     toggleDropDownDisplay,
+//     toggleDropDownIcon,
+//   };
+// })();
 
 const events = (() => {
-  const addHeaderLinkEvents = () => {
-    const headerLinks = document.querySelectorAll(".header-link");
-    headerLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        const dropIcon = link.nextElementSibling;
-        headerContent.toggleDropDownDisplay(link.getAttribute("id"));
-        headerContent.toggleDropDownIcon(dropIcon);
+  const addModalDropMenuEvent = () => {
+    const headerTopLinks = document.querySelectorAll(".header-top-links");
+    headerTopLinks.forEach((link) => {
+      link.addEventListener("mouseenter", (e) => {
+        e.target.querySelector(".modal-menu").classList.add("modal-menu-active");
+        e.target.classList.add("arrow-active");
+      });
+      link.addEventListener("mouseleave", (e) => {
+        e.target.querySelector(".modal-menu").classList.remove("modal-menu-active");
+        e.target.classList.remove("arrow-active");
       });
     });
   };
 
   return {
-    addHeaderLinkEvents,
+    addModalDropMenuEvent,
   };
 })();
 
 // buildHeaderContent
 (() => {
-  events.addHeaderLinkEvents();
+  events.addModalDropMenuEvent();
   // const logoContent = document.query
   // const headerDiv = document.createElement("header");
   // headerDiv.setAttribute("id", "header-content");
